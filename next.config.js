dd41741b0e9e52a3d3526243c6efb5393ca150f9
sagarvedverma <sagarvedverma@gmail.com>
2026-02-23 23:19:55 +0530

    add portfolio as project context.

--- next.config.js
+++ next.config.js
@@ -1,7 +1,11 @@
 /** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === 'production';
+
 const nextConfig = {
   output: 'export',
   trailingSlash: true,
+  basePath: isProd ? '/portfolio' : '',
+  assetPrefix: isProd ? '/portfolio/' : '',
   eslint: {
     ignoreDuringBuilds: true,
   },
